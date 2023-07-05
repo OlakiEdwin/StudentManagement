3d582b60219e2bb27d8acc3517192b58bae58f4a
Olaki Edwin <olakiedwin@gmail.com>
2023-07-05 15:22:58 +0300

    Imports changed

--- src/main/java/com/example/StudentMangementSystem/entity/Student.java
+++ src/main/java/com/example/StudentMangementSystem/entity/Student.java
@@ -1,8 +1,13 @@
 package com.example.StudentMangementSystem.entity;
 
 
-import jakarta.persistence.*;
-//javax.persistence.*
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
 
 @Entity
 @Table(name = "students")
